9f12adb98b104b70ac1e17d1085a5b9632ad7dc1
nkamble1985 <113365112+nkamble1985@users.noreply.github.com>
2025-08-20 10:23:32 +0000

    Add files via upload

---
+++ RSHDeployments/RiskModel/script/IIQ_RSH_Market_Return_Stat.ps1
@@ -0,0 +1,164 @@
+﻿#Log file path
+$Logfile = "c:\Logs\Log_EXEC_Holdings.txt";
+$SummaryLogfile = "c:\Logs\EXEC_HoldingsSummaryLog.txt";
+$jobname = "EXEC_Holdings"
+
+#Used for Recursive Script run
+$script:counter = 1;
+$script:exitcounter = 1;
+$timeInterval = 30; # in seconds
+
+#SMTP Details
+$smtpUsername = "Alerts_Research@IndexIQ.com";
+$smtpPassword = "Connect@2022";
+$toEmail = "mkim@indexiq.com,munawar.gani@ap.linedata.com,nitin.kamble@ap.linedata.com,index@indexiq.com,alerts@gravitas.co";
+$errorEmail = "munawar.gani@ap.linedata.com,nitin.kamble@ap.linedata.com";#
+
+
+
+
+$successMessage = "Successfully executed the Cosmos Holdings statistic program.";
+$errorMessage = "";
+
+#Function to be used for logging
+Function LogWrite
+{
+	Param ([string]$logstring)
+	$DateStr = Get-Date -Format "MM/dd/yyyy HH:mm:ss "
+	Add-content $Logfile -value "$DateStr - $logstring"
+}
+
+#Function to be used for logging summary
+Function SummaryLogWrite
+{
+	Param ([string]$logstring)
+	$DateStr = Get-Date -Format "MM/dd/yyyy HH:mm:ss "
+	Add-content $SummaryLogfile -value "$DateStr - $logstring"
+}
+
+#Function to be used for sending email
+function Send-ToEmail([string]$email, [string]$emailmessage)
+{
+	try{
+			$message = new-object Net.Mail.MailMessage;
+			$message.From = $smtpUsername;
+			$message.To.Add($email);
+			$message.Subject = "Market Returns Data Alert";
+            $message.IsBodyHTML = $true
+			$message.Body = $emailmessage;
+			
+			$smtp = new-object Net.Mail.SmtpClient("smtp.office365.com", "587");
+			$smtp.EnableSSL = $true;
+            
+			$smtp.Credentials = New-Object System.Net.NetworkCredential($smtpUsername, $smtpPassword);
+			#$smtp.send($message);
+		}
+	catch{
+			$errorMessage = $($_.Exception.Message);
+			LogWrite("Send-ToEmail - Exception Message: $($_.Exception.Message)")
+		}
+ }
+ 
+ function Get-LastBusinessDay {
+    [CmdletBinding()]
+    param (
+        [Parameter(Position = 1)]
+        [System.DateTime]$Date = [System.DateTime]::Today,
+        [Parameter(Position = 2)]
+        [System.String]$DateFormat
+    );
+
+    $Weekends = @([System.DayOfWeek]::Saturday, [System.DayOfWeek]::Sunday);
+	$LastBusinessDay = $Date.AddDays(-1);
+
+    while ($LastBusinessDay.DayOfWeek -in $Weekends) {
+        $LastBusinessDay = $LastBusinessDay.AddDays(-1);
+    }
+
+    return $LastBusinessDay.ToString($DateFormat);
+}
+
+
+LogWrite("Job Start ***********************************************************");
+SummaryLogWrite("$jobname Log Starts ------------------------------------------------------------------------------------------------------------------------------");
+
+#Loop after the defined interval to check if file exists
+while($script:counter -le $script:exitcounter) {
+	LogWrite("Starting counter - $script:counter");
+	try{
+			$days = 0;
+			$runDate = Get-LastBusinessDay -Date (Get-Date).AddDays($days) -DateFormat "yyyyMMdd"
+			LogWrite("Run Date --> $runDate");
+			
+            #$Pricerrex = Invoke-Sqlcmd -ServerInstance "inviiqresearch-sqlserver-standard-dev.ckryme4eosdx.us-east-1.rds.amazonaws.com" -Database "ResearchDWH" -Query "EXEC GetTotallReturnStatData" -QueryTimeout 0 
+            
+			#Send-ToEmail -email "$toEmail" -emailmessage $Pricerrex[0];
+			
+			
+			$connectionString = "Server=inviiqresearch-sqlserver-standard-dev.ckryme4eosdx.us-east-1.rds.amazonaws.com;Database=ResearchDWH;User Id=stonebranchuser;Password=Welcome@2025;TrustServerCertificate=True;"
+
+            $avgdata = Invoke-Sqlcmd -ConnectionString $connectionString -Query "EXEC GetTotallReturnStatData_Avrage_Updated" -QueryTimeout 0
+
+           
+            $avgtable = "<html><style> th {border:2px black solid !important} </style> <style> td {border:2px black solid !important} </style>
+             <style> table {border:2px black solid !important} </style><table>"
+            $avgtable += "<tr>"
+            $avgtable += "<th>N</th>"
+            $avgtable += "<th>maxRetPct</th>"
+            $avgtable += "<th>minRetPct</th>"
+            $avgtable += "</tr>"
+            
+            foreach ($row in $avgdata) {
+                $avgtable += "<tr>"
+                $avgtable += "<td>$($row.N)</td>"
+                $avgtable += "<td>$($row.maxRetPct)</td>"
+                $avgtable += "<td>$($row.minRetPct)</td>"
+                $avgtable += "</tr>"
+            }
+            
+            $avgtable += "</table></br></br>"
+
+
+           $connectionString = "Server=inviiqresearch-sqlserver-standard-dev.ckryme4eosdx.us-east-1.rds.amazonaws.com;Database=ResearchDWH;User Id=stonebranchuser;Password=Welcome@2025;TrustServerCertificate=True;"
+
+           $returndata = Invoke-Sqlcmd -ConnectionString $connectionString -Query "EXEC GetTotallReturnStatData_Updated" -QueryTimeout 0
+
+           
+            $returntable = "<table>"
+            $returntable += "<tr>"
+            $returntable += "<th>Ticker</th>"
+            $returntable += "<th>DailyReturn</th>"
+            $returntable += "<th>CreatedOn</th>"
+            $returntable += "</tr>"
+            
+            foreach ($row in $returndata) {
+                $returntable += "<tr>"
+                $returntable += "<td>$($row.Ticker)</td>"
+                $returntable += "<td>$($row.DailyReturn)</td>"
+                $returntable += "<td>$($row.CreatedOn)</td>"
+                $returntable += "</tr>"
+            }
+            
+            $returntable += "</table></html>"
+
+            $table =$avgtable+$returntable
+
+           
+           write-host $table2
+		   #LogWrite("Data: $table2");
+           #Send-ToEmail -email "$toEmail" -emailmessage $table;
+           
+           return $table
+		}
+	catch{
+			$errorMessage = $($_.Exception.Message);
+			LogWrite("Exception Message: $($_.Exception.Message)");
+			SummaryLogWrite("Exception Message: $($_.Exception.Message)");
+			#Send-ToEmail -email "$errorEmail" -emailmessage "$errorMessage";
+		}
+	Start-Sleep -Seconds $timeInterval
+	LogWrite("Ending counter - $script:counter");
+	$script:counter += 1
+}
+LogWrite("Job End ***********************************************************");
+SummaryLogWrite("$jobname Log Ends ------------------------------------------------------------------------------------------------------------------------------");
